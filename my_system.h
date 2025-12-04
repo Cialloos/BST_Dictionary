15effcc712721dc807bac99c0169bc87fd0931ac
Ciallo <2267728389@qq.com>
2025-12-04 16:12:02 +0800

    upgreade

--- my_system.h
+++ my_system.h
@@ -1,24 +1,52 @@
 #pragma once
 #include "my_BST.h"
-
+#include<fstream>
+#include<sstream>
 class dicsys {
 private:
 	BST tree;
 public:
-	bool add() {}
+	void add(string w, string m) {
+		tree.add(w, m);
+	}
 
-	bool remove() {}
+	bool remove(string w) {
+		return tree.remove(w);
+	}
 
-	string find() {}
+	bool revise(string w,string m) {
+		node* ptr = tree.find(w);
+		if (ptr != nullptr) {
+			ptr->setmeaning(m);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
+	node* find(string w) {
+		return tree.find(w);
+	}
 
+	void addByFile() {
+		ifstream file("data.txt");
+		if (!file) {
+			cerr << "file open failed for reading";
+			exit(1);
+		}
+		string line;
+		while (getline(file, line)) {
+			istringstream iss(line);
+			string w, m;
+			iss >> w >> m;
+			tree.add(w, m);
+		}
+		file.close();
+	}
 
+	void showStruct() {
+		tree.show();
+	}
 
-
-
-
-
-	void addByFile() {}
-
-	void showStruct() {}
 };
--- test01.cpp
+++ test01.cpp
@@ -1,4 +1,5 @@
-/*
-
-
-*/
+#include "my_usersys.h"
+int main() {
+	user u;
+	u.start();
+}
